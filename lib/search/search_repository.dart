3e65a0f6a4c9c5a23290584c2b5ddc14e81fb181
koray <koraymetin@windowslive.com>
2024-03-10 20:47:27 +0300

    create the test env. for search_repository api

--- lib/search/search_repository.dart
+++ lib/search/search_repository.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import '../api_layer/base_service.dart';
 
 class SearchRepository extends BaseService {
@@ -7,7 +6,7 @@ class SearchRepository extends BaseService {
 
   Future<Response> fetchResultsBy({required String searchQuery}) async {
     // jack+johnson
-    return await client.get("https://itunes.apple.com/search?term=$searchQuery");
+    return await client.get("/search?term=$searchQuery");
   }
 
 }
---
+++ test/search_tests/search_repository_test.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
+import 'package:itunessearchbloc/api_layer/itunes_interceptor.dart';
+import 'package:itunessearchbloc/search/search_repository.dart';
+import 'package:itunessearchbloc/api_layer/itunes_search_api_response.dart';
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  late Dio dio;
+  late DioAdapter dioAdapter;
+  const baseUrl = 'https://example.com';
+  setUp(() {
+    dio = Dio(BaseOptions(baseUrl: baseUrl));
+    dio.interceptors.add(ItunesInterceptor());
+    dioAdapter = DioAdapter(dio: dio);
+  });
+
+  test("mock search?term endpoint on fetchResultsBy", () async {
+    String successPath = "test/assets/jack_johnson_results.json";
+    String responseString = await rootBundle.loadString(successPath);
+
+    const query = "jack+johnson";
+    dioAdapter.onGet("/search?term=$query", (server) => server.reply(
+      200,
+      responseString,
+      delay: const Duration(seconds: 1),
+    ));
+
+    SearchRepository searchRepository = SearchRepository(dio: dio);
+    var searchResponse = await searchRepository.fetchResultsBy(searchQuery: query);
+    expect(searchResponse.data is ItunesSearchAPIResponseData, true);
+    ItunesSearchAPIResponseData data = searchResponse.data;
+    expect(data.resultCount, 49);
+    expect(data.results.first.trackName, "Star Wars: The Force Awakens");
+  });
+}
