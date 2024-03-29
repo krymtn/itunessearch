06ab9fd66b2cff6b932b4963df89ba29c11bf03a
koray <koraymetin@windowslive.com>
2024-03-29 16:50:38 +0300

    prepare album dto with tests

--- lib/results/models/models.dart
+++ lib/results/models/models.dart
@@ -3,3 +3,4 @@ export 'album.dart';
 export 'song.dart';
 export 'result_dto.dart';
 export 'entity.dart';
+export 'music_video.dart';
--- test/results/repository_test.dart
+++ test/results/repository_test.dart
@@ -21,12 +21,12 @@ void main() {
   group("mock/kuzukuzu_songs.json results", () {
     String successPath = "test/assets/results/kuzukuzu_songs.json";
     const query = "kuzu+kuzu";
-    const entity = "song";
+    String entityValue = Entities.song.value;
     late dio_client.Response songResponse;
 
     setUp(() async {
       String responseString = await rootBundle.loadString(successPath);
-      dioAdapter.onGet("/search?term=$query&entity=$entity&limit=15", (server) => server.reply(
+      dioAdapter.onGet("/search?term=$query&entity=$entityValue&limit=15", (server) => server.reply(
         200,
         responseString,
         delay: const Duration(seconds: 1),
@@ -52,30 +52,60 @@ void main() {
   group("mock/kuzukuzu_albums.json results", () {
     String successPath = "test/assets/results/kuzukuzu_albums.json";
     const query = "kuzu+kuzu";
-    const entity = "album";
-    late dio_client.Response songResponse;
+    String entityValue = Entities.album.value;
+    late dio_client.Response albumResponse;
 
     setUp(() async {
       String responseString = await rootBundle.loadString(successPath);
-      dioAdapter.onGet("/search?term=$query&entity=$entity&limit=15", (server) => server.reply(
+      dioAdapter.onGet("/search?term=$query&entity=$entityValue&limit=15", (server) => server.reply(
         200,
         responseString,
         delay: const Duration(seconds: 1),
       ));
       ResultsRepository repository = ResultsRepository(dio: dio);
-      songResponse = await repository.fetchOverviewSearchItemsBy(entity: Entities.album, queryText: query);
+      albumResponse = await repository.fetchOverviewSearchItemsBy(entity: Entities.album, queryText: query);
     });
 
     test("1. Check the pure response data", () {
-      expect(songResponse.data is ItunesSearchAPIResponse, true);
-      ItunesSearchAPIResponse itunesSearchAPIResponse = songResponse.data;
+      expect(albumResponse.data is ItunesSearchAPIResponse, true);
+      ItunesSearchAPIResponse itunesSearchAPIResponse = albumResponse.data;
       expect(itunesSearchAPIResponse.resultCount, 15);
     });
 
     test("2. album constructor with json", () {
-      ItunesSearchAPIResponse itunesSearchAPIResponse = songResponse.data;
+      ItunesSearchAPIResponse itunesSearchAPIResponse = albumResponse.data;
       List<Album> songs = itunesSearchAPIResponse.results.map((e) => Album.fromJson(e)).toList();
       expect(songs.first.collectionName, "Kuzu Kuzu (Remixes) - EP");
+    });
+  });
+
+  group("mock/kuzukuzu_musicvideos.json results", () {
+    String successPath = "test/assets/results/kuzukuzu_musicvideos.json";
+    const query = "kuzu+kuzu";
+    String entityValue = Entities.musicVideo.value;
+    late dio_client.Response musicVideoResponse;
+
+    setUp(() async {
+      String responseString = await rootBundle.loadString(successPath);
+      dioAdapter.onGet("/search?term=$query&entity=$entityValue&limit=15", (server) => server.reply(
+        200,
+        responseString,
+        delay: const Duration(seconds: 1),
+      ));
+      ResultsRepository repository = ResultsRepository(dio: dio);
+      musicVideoResponse = await repository.fetchOverviewSearchItemsBy(entity: Entities.musicVideo, queryText: query);
+    });
+
+    test("1. Check the pure response data", () {
+      expect(musicVideoResponse.data is ItunesSearchAPIResponse, true);
+      ItunesSearchAPIResponse itunesSearchAPIResponse = musicVideoResponse.data;
+      expect(itunesSearchAPIResponse.resultCount, 2);
+    });
+
+    test("2. album constructor with json", () {
+      ItunesSearchAPIResponse itunesSearchAPIResponse = musicVideoResponse.data;
+      List<MusicVideo> musicVideos = itunesSearchAPIResponse.results.map((e) => MusicVideo.fromJson(e)).toList();
+      expect(musicVideos.first.artistName, "t-Ace");
     });
   });
 }
