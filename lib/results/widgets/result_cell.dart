83c9cbcee1cd597bfbde4d85a8d74b7ef60a9046
koray <koraymetin@windowslive.com>
2024-04-01 18:20:50 +0300

    common cell improvements

--- lib/results/widgets/result_cell.dart
+++ lib/results/widgets/result_cell.dart
@@ -8,31 +8,26 @@ abstract class ResultCell<T extends ResultDTO> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: const BoxConstraints(
-          maxWidth: 100
-      ),
+      constraints: const BoxConstraints(maxWidth: 100),
       child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [image, track, artist]
-      ),
+          children: [image, track, artist]),
     );
   }
 
   Image get image => Image.network(item.artworkUrl100.toString(), fit: BoxFit.fitWidth);
   Text get track => Text(item.trackName.toString(), maxLines: 2, overflow: TextOverflow.ellipsis);
   Text get artist => Text(item.artistName.toString(), maxLines: 1, style: const TextStyle(fontWeight: FontWeight.w700));
-  Widget get price => (item.price != null) ?Container(
-    width: 70,
-    decoration: BoxDecoration(
-      border: Border.all(width: 1.0, color: Colors.blue),
-      borderRadius: const BorderRadius.all(Radius.circular(5.0))
-    ),
-    child: Padding(
-      padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0, bottom: 4.0),
-      child: Text("₺${item.price}", textAlign: TextAlign.center, style: const TextStyle(color: Colors.blue, fontWeight: FontWeight.w700)),
-    ),
-  ):const SizedBox.shrink();
+  Widget get price => (item.price != null)
+      ? Container(
+          width: 70,
+          decoration: BoxDecoration(border: Border.all(width: 1.0, color: Colors.blue), borderRadius: const BorderRadius.all(Radius.circular(5.0))),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0, bottom: 4.0),
+            child: Text("₺${item.price}", textAlign: TextAlign.center, style: const TextStyle(color: Colors.blue, fontWeight: FontWeight.w700)),
+          ),
+        )
+      : const SizedBox.shrink();
 }
-
